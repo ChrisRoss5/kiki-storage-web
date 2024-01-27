9b9ad68c0b2f7540c8e2439043d2a3e600beb90a
Chris <kristijan.ros@gmail.com>
2024-01-27 16:00:09 +0100

    mini update

--- web-client/src/components/explorer/CloseExplorer.vue
+++ web-client/src/components/explorer/CloseExplorer.vue
@@ -3,7 +3,7 @@
     class="z-10 cursor-pointer whitespace-nowrap py-4 text-center text-base-content opacity-50 transition-opacity hover:opacity-100 lg:py-2"
   >
     <span class="material-symbols-outlined"> expand_less </span>
-    {{ $breakpoints.lgAndUp ? "Press esc or click" : "Tap" }} here to close
+    {{ $inputMechanism.isCoarse ? "Tap" : "Press esc or click" }} here to close
     <span class="material-symbols-outlined"> expand_less </span>
   </div>
 </template>
